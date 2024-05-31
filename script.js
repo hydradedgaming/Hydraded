87a9f53a146ff250a986e5518e776c007fb4e6b5
hydradedgaming <167041346+hydradedgaming@users.noreply.github.com>
2024-05-31 10:22:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 function redirectTo(url) {
     window.location.href = url;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const images = [
+        'url1.jpg',
+        'url2.jpg',
+        'url3.jpg'
+    ];
+
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+    document.body.style.backgroundImage = `url(${randomImage})`;
+});
